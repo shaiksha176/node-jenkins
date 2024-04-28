ac44cc184db3aa8fb23e6a350cbeb84fb262facf
shaiksha176 <iamshaiksha17@gmail.com>
2024-04-28 13:25:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 5000;
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors("*"));
+
+// ROUTES
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send({ message: "Welcome to the Node and jenkins Applicatin" });
+});
+
+app.get("/status", (req, res) => {
+  res.status(201).send({ message: "API is working fine!!!" });
+});
+
+app.use("*", (req, res) => {
+  res.status(404).send({
+    message: "No route exists with the specified path",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
